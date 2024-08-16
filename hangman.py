3135c9aa3436627c90849c2123df6a9ee225ad15
Oliver HJ <157483521+MeowMeow74@users.noreply.github.com>
2024-08-16 14:25:10 -0400

    hangman2.py
    updated hangman correct

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+
 word = input('enter a word for p2 to guess: ')
 
 max_guess = len(word)
@@ -10,6 +11,7 @@ i = 0
 string_holder = []
 string_holder.extend('_' * len(word))
 
+print(type(word))
 print(f'you have {len(word)} letters to guess')
 
 # iterate for each letter in the word
@@ -18,29 +20,32 @@ print(f'you have {len(word)} letters to guess')
 while wrong_guess >= max_guess and i < max_guess:
   # while :
 
+    user_char = input('Enter a letter: ')
+    # fist check for duplicate values in the string_holder
+    if (user_char.lower()) in string_holder:
+        print('you have already guessed that')
 
-    user_char = input('Enter a guess: ')
 
-    #check if user inputed charachter is equal to pos of correct word
-    # but if you get one wrong it needs to remeber pos of word
-    # here i will not count up towards main while loop and will iterate as long ase
-    # i is less than max_guess
-    if user_char == word[i]:
-      # need function to replace letters in string
-      string_holder[i] = user_char
+    # check for correct values and replace any instance of a correct char
+    elif (user_char.lower()) in word:
+      for letter in range(len(word)):
+        if word[letter] == user_char:
+            string_holder[letter] = user_char
       i += 1
       print('correct_guess', string_holder)
 
+    # this reset pos in word check if you get incorrect
     else:
-      # this reset pos in word check if you get incorrect
       wrong_guess -= 1
-      print(string_holder)
-
-      # we correcting math here
       if wrong_guess - len(word) + 1 == 0:
         print(f'eeer.  no more guesses left.  word was {word}')
       else:
         print(f'incorrect, you have {wrong_guess - len(word) + 1} guesses left')
         print('try again\n')
 
+    # final gamewinning check
+    if ''.join(string_holder) == ''.join(word):
+      i = 10000
+      print('Congrats, you guessed the word')
+# done
 
